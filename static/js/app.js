f6ea6fc4db152b3b52214daee78d08b16113e4d7
Tyson <tyson@horsewell.me>
2024-02-25 12:29:02 +0800

    Update app.js
    Added blubble plots

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,8 @@
 const debug = true;
+const bgColour = 'RoyalBlue';
+const fontColour = 'white';
+
+document.body.style.backgroundColor = bgColour;
 
 
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
@@ -26,10 +30,9 @@ d3.json(url).then(function (data) {
         let barz = data.samples[itemIdx].otu_labels.slice(0, 10).reverse();//otu_labels
 
         if (debug) { console.log(barx); console.log(bary); console.log(barz); }
-        
+
         //color options
-        let colorlist = ['red', 'orangered', 'orange', 'yellow', 'yellowgreen', 'green', 'blue', 'mediumblue', 'rebeccapurple', 'indigo'];//pairs with Jet and Portland
-        let colorlist2 = ['#f0f921', '#fdca26', '#fb9f3a', '#ed7953', '#d8576b', '#bd3786', '#9c179e', '#7201a8', '#46039f', '#0d0887']//reverse plasma
+        let colours = ["#FFFF00", "#FFCC00", "#FF9900", "#FF6600", "#FF3300", "#FF0000", "#CC0000", "#990000", "#660000", "#330000"];
 
         let trace1 = {
             x: barx,
@@ -37,7 +40,7 @@ d3.json(url).then(function (data) {
             text: barz,
             type: 'bar',
             orientation: 'h',
-            marker: { color: colorlist2 }
+            marker: { color: colours }
         };
         let data1 = [trace1];
 
@@ -48,12 +51,53 @@ d3.json(url).then(function (data) {
             width: 500,
             xaxis: { title: "OTU count" },
             margin: { t: 50, r: 25, l: 75, b: 35 },
-            paper_bgcolor: "royalblue",
-            font: { color: "white", family: "Arial" }
+            paper_bgcolor: bgColour,
+            font: { color: fontColour, family: "Arial" }
         };
 
         Plotly.newPlot("bar", data1, layout1)
     };
     barChart(itemIdx);
+
+    // Crete a bubble plot visual
+    // Plots all of the Operational Taxonomic Units (otu_id) on the x axis and by marker color
+    // and count(sample_value) on the y axis and by marker size
+    // additional hover info shows the types of bacteria found (otu_labels) for the default or selected Test Subject ID
+
+    function bubbleChart(itemIdx) {
+        let bubblex = [data.samples[itemIdx].otu_ids];
+        let bubbley = [data.samples[itemIdx].sample_values];
+        let bubblez = [data.samples[itemIdx].otu_labels];
+
+        if (debug) { console.log(bubblex); console.log(bubbley); console.log(bubblez); }
+
+        let trace2 = {
+            x: bubblex[0],
+            y: bubbley[0],
+            xlimit: 4000,
+            text: bubblez[0],
+            mode: 'markers',
+            marker: {
+                size: bubbley[0],
+                color: bubblex[0],
+                colorscale: 'Electric'
+            }
+        };
+        let data2 = [trace2];
+
+        let layout2 = {
+            title: { text: `ID_${data.names[itemIdx]} OTU Counts`, font: { size: 24 } },
+            showlegend: false,
+            margin: { t: 75, r: 25, l: 50, b: 35 },
+            paper_bgcolor: bgColour,
+            font: { color: fontColour, family: "Arial" },
+            xaxis: { title: "OTU_ID" },
+            yaxis: { title: "OTU count" }
+        };
+
+        Plotly.newPlot('bubble', data2, layout2)
+    };
+    bubbleChart(itemIdx);
+
 
 });
